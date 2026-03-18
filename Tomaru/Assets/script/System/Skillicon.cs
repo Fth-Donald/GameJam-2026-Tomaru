cf7e0028b2d73e8e1b7ed6c189fc9d1eb8069e9f
1mmorta12345 <xuerenhao30@gmail.com>
2026-03-18 16:59:47 +0900

    Update Skillicon.cs

--- Tomaru/Assets/script/System/Skillicon.cs
+++ Tomaru/Assets/script/System/Skillicon.cs
@@ -1,10 +1,42 @@
+﻿//using UnityEngine;
+
+//public class Skillicon : MonoBehaviour
+//{
+//    [Header("Skill Prefabs")]
+//    public GameObject[] SkillPrefabs;
+//    [Header("Player Prefab")]
+//    public PlayerController playerController;
+
+//}
+
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Skillicon : MonoBehaviour
 {
     [Header("Skill Prefabs")]
     public GameObject[] SkillPrefabs;
+
+    [Header("Skill Icons")]
+    public Sprite[] skillIcons;
+
     [Header("Player Prefab")]
     public PlayerController playerController;
 
+    private Image iconImage;
+
+    private void Awake()
+    {
+        iconImage = GetComponent<Image>();
+    }
+    private void Update()
+    {
+        SetSkillIcon(playerController.skill1);
+    }
+    public void SetSkillIcon(int index)
+    {
+        int SkillIndex = playerController.skill1;
+        iconImage.sprite = skillIcons[SkillIndex];
+    }
 }
