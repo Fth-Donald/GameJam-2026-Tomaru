35251ce68fd7d6ac21cb0baf2a20f289116022c8
1mmorta12345 <xuerenhao30@gmail.com>
2026-03-18 16:38:58 +0900

    update

--- Tomaru/Assets/script/System/Skillicon.cs
+++ Tomaru/Assets/script/System/Skillicon.cs
@@ -4,4 +4,7 @@ public class Skillicon : MonoBehaviour
 {
     [Header("Skill Prefabs")]
     public GameObject[] SkillPrefabs;
+    [Header("Player Prefab")]
+    public PlayerController playerController;
+
 }
