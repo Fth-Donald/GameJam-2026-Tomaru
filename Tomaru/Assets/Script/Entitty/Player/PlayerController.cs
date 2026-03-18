9234216164e83502fc62cbea3fa03f7c1832db5e
1mmorta12345 <xuerenhao30@gmail.com>
2026-03-18 13:20:47 +0900

    wave enemy list update

--- Tomaru/Assets/Script/Entitty/Player/PlayerController.cs
+++ Tomaru/Assets/Script/Entitty/Player/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : Entity
     [SerializeField] GameObject sword;
     private bool usingBarrier = true;
 
-    public Image healthBar;
+    public HPScript hPScript;
 
     [Header("Movement")]
     
