5c84e6e5c121269c27ca3e43be80dac59d407ddf
1mmorta12345 <xuerenhao30@gmail.com>
2026-03-18 15:59:59 +0900

    update

--- Tomaru/Assets/script/Entitty/Player/HPScript.cs
+++ Tomaru/Assets/script/Entitty/Player/HPScript.cs
@@ -14,8 +14,6 @@ public class HPScript : MonoBehaviour
         cH = playerController.currentHealth;
         mH = playerController.maxHealth;
         fillImage.fillAmount = cH / mH;
-        Debug.Log("ph"+playerController.currentHealth);
-        Debug.Log("mh"+playerController.maxHealth);
     }
 }
 //
